72742f6daf2d2aee379b1f6939572a25fa57d5fb
Yahya NAJAR <yahya.najar@gmail.com>
2017-03-13 12:16:24 +0100

    migration of xc.bench

--- Examples/xc.bench/benchsimplefork/BenchSimpleFork/TriggeredMethod/TransitionTriggerTriggeredMethod.cs
+++ Examples/xc.bench/benchsimplefork/BenchSimpleFork/TriggeredMethod/TransitionTriggerTriggeredMethod.cs
@@ -1,24 +1,31 @@
+using System;
+using XComponent.BenchSimpleFork.Common;
+using XComponent.BenchSimpleFork.Common.Senders;
+using XComponent.Common.ApiContext;
+using XComponent.Common.Timeouts;
+
 namespace XComponent.BenchSimpleFork.TriggeredMethod
 {
-	using System;
-	using XComponent.Common.ApiContext;
-	using XComponent.Common.Timeouts;
-	using XComponent.BenchSimpleFork.Common.Senders;
-	using XComponent.BenchSimpleFork.Common;
+    using System;
+    using XComponent.Common.ApiContext;
+    using XComponent.Common.Timeouts;
+    using XComponent.BenchSimpleFork.Common.Senders;
+    using XComponent.BenchSimpleFork.Common;
 
 
-	public static class TransitionTriggerTriggeredMethod
-	{
+    public static class TransitionTriggerTriggeredMethod
+    {
 
-		/// <summary>
-		/// Executing triggeredMethod ExecuteOn_Done_Through_TransitionTrigger
-		/// </summary>
-		public static void ExecuteOn_Done_Through_TransitionTrigger(XComponent.BenchSimpleFork.UserObject.TransitionTrigger transitionTrigger, object object_PublicMember, object object_InternalMember, Context context, ITransitionTriggerTransitionTriggerOnDoneTransitionTriggerSenderInterface sender)
-		{
-			TriggeredMethodContext.Instance.StartBench();
-			for (int i = 0; i < transitionTrigger.NbInstances; i++) {
-				sender.SendEvent(StdEnum.TriggeringRuleBench, new UserObject.TriggerTransition { Id = i });
-			}
-		}
-	}
+        /// <summary>
+        /// Executing triggeredMethod ExecuteOn_Done_Through_TransitionTrigger
+        /// </summary>
+        public static void ExecuteOn_Done_Through_TransitionTrigger(XComponent.BenchSimpleFork.UserObject.TransitionTrigger transitionTrigger, object object_PublicMember, object object_InternalMember, Context context, ITransitionTriggerTransitionTriggerOnDoneTransitionTriggerSenderInterface sender)
+        {
+            TriggeredMethodContext.Instance.StartBench();
+            for (int i = 0; i < transitionTrigger.NbInstances; i++)
+            {
+                sender.SendEvent(StdEnum.TriggeringRuleBench, new UserObject.TriggerTransition { Id = i });
+            }
+        }
+    }
 }
--- Examples/xc.bench/benchsimplefork/BenchSimpleFork/TriggeredMethod/TriggeringRuleBenchTriggeredMethod.cs
+++ Examples/xc.bench/benchsimplefork/BenchSimpleFork/TriggeredMethod/TriggeringRuleBenchTriggeredMethod.cs
@@ -1,40 +1,49 @@
+using System;
+using XComponent.BenchSimpleFork.Common;
+using XComponent.BenchSimpleFork.Common.Senders;
+using XComponent.Common.ApiContext;
+using XComponent.Common.Timeouts;
+
 namespace XComponent.BenchSimpleFork.TriggeredMethod
 {
-	using System;
-	using XComponent.Common.ApiContext;
-	using XComponent.Common.Timeouts;
-	using XComponent.BenchSimpleFork.Common.Senders;
-	using XComponent.BenchSimpleFork.Common;
-	using UserObject;
+    using System;
+    using XComponent.Common.ApiContext;
+    using XComponent.Common.Timeouts;
+    using XComponent.BenchSimpleFork.Common.Senders;
+    using XComponent.BenchSimpleFork.Common;
+    using UserObject;
 
-	public static class TriggeringRuleBenchTriggeredMethod
-	{
+    public static class TriggeringRuleBenchTriggeredMethod
+    {
 
-		/// <summary>
-		/// Executing triggeredMethod ExecuteOn_Done_Through_TriggerTransition
-		/// </summary>
-		public static void ExecuteOn_Done_Through_TriggerTransition(XComponent.BenchSimpleFork.UserObject.TriggerTransition triggerTransition, object object_PublicMember, XComponent.BenchSimpleFork.UserObject.TriggeringRuleBench triggeringRuleBench, Context context, ITriggerTransitionTriggerTransitionOnDoneTriggeringRuleBenchSenderInterface sender)
-		{
-			TriggeredMethodContext.Instance.IncrementInstances();
-			if (triggeringRuleBench.IsLast) {
-				TriggeredMethodContext.Instance.StopBench();
-				sender.SendEvent(StdEnum.BenchManager, new BenchResult {
-					TotalTimeMilliseconds = TriggeredMethodContext.Instance.TotalTimeMilliseconds,
-					NbInstances = TriggeredMethodContext.Instance.NbInstances
-				});
-			}
-		}
+        /// <summary>
+        /// Executing triggeredMethod ExecuteOn_Done_Through_TriggerTransition
+        /// </summary>
+        public static void ExecuteOn_Done_Through_TriggerTransition(XComponent.BenchSimpleFork.UserObject.TriggerTransition triggerTransition, object object_PublicMember, XComponent.BenchSimpleFork.UserObject.TriggeringRuleBench triggeringRuleBench, Context context, ITriggerTransitionTriggerTransitionOnDoneTriggeringRuleBenchSenderInterface sender)
+        {
+            TriggeredMethodContext.Instance.IncrementInstances();
+            if (triggeringRuleBench.IsLast)
+            {
+                TriggeredMethodContext.Instance.StopBench();
+                sender.SendEvent(StdEnum.BenchManager, new BenchResult
+                {
+                    TotalTimeMilliseconds = TriggeredMethodContext.Instance.TotalTimeMilliseconds,
+                    NbInstances = TriggeredMethodContext.Instance.NbInstances
+                });
+            }
+        }
 
-		/// <summary>
-		/// Executing triggeredMethod ExecuteOn_Benching_Through_CreateInstance
-		/// </summary>
-		public static void ExecuteOn_Benching_Through_CreateInstance(XComponent.BenchSimpleFork.UserObject.CreateInstance createInstance, object object_PublicMember, XComponent.BenchSimpleFork.UserObject.TriggeringRuleBench triggeringRuleBench, Context context, ICreateInstanceCreateInstanceOnBenchingTriggeringRuleBenchSenderInterface sender)
-		{
-			triggeringRuleBench.Id = createInstance.Id;
-			triggeringRuleBench.IsLast = createInstance.IsLast;
-			if (triggeringRuleBench.IsLast) {
-				sender.TransitionTrigger(context, new TransitionTrigger { NbInstances = triggeringRuleBench.Id + 1 });
-			}
-		}
-	}
+        /// <summary>
+        /// Executing triggeredMethod ExecuteOn_Benching_Through_CreateInstance
+        /// </summary>
+        public static void ExecuteOn_Benching_Through_CreateInstance(XComponent.BenchSimpleFork.UserObject.CreateInstance createInstance, object object_PublicMember, XComponent.BenchSimpleFork.UserObject.TriggeringRuleBench triggeringRuleBench, Context context, ICreateInstanceCreateInstanceOnBenchingTriggeringRuleBenchSenderInterface sender)
+        {
+            triggeringRuleBench.Id = createInstance.Id;
+            triggeringRuleBench.IsLast = createInstance.IsLast;
+            if (triggeringRuleBench.IsLast)
+            {
+                sender.TransitionTrigger(context, new TransitionTrigger { NbInstances = triggeringRuleBench.Id + 1 });
+            }
+        }
+    }
 }
