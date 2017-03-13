aa31ea6f018cffebc53c7d772a4a3cfb5b87906b
Yahya NAJAR <yahya.najar@gmail.com>
2017-03-13 12:07:02 +0100

    migration of xc.slack

---
+++ Examples/xc.slack/slackproject/SlackProxy/Common/EventHashCodeCalculator/SlackProxyEventHashCodeCalculatorRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using XComponent.Engine.Execution.HashCode;
+
+namespace XComponent.SlackProxy.Common.EventHashCodeCalculator
+{
+	public class SlackProxyEventHashCodeCalculatorRepository : IEventHashCodeCalculatorRepository
+	{
+		Dictionary<int, IEventHashCodeCalculator> _calculators;
+
+		public SlackProxyEventHashCodeCalculatorRepository()
+		{
+			_calculators = new Dictionary<int, IEventHashCodeCalculator>();
+			_calculators.Add(6, new XComponent.SlackProxy.Common.EventHashCodeCalculator.SlackProxy.DefaultEventEventHashCodeCalculator());
+			_calculators.Add(10, new XComponent.SlackProxy.Common.EventHashCodeCalculator.SlackProxy.SendMessageEventHashCodeCalculator());
+		}
+
+		public Dictionary<int, IEventHashCodeCalculator> GetEventHashCodeCalculators()
+		{
+			return _calculators;
+		}
+	}
+}
--- Examples/xc.slack/slackproject/SlackProxy/TriggeredMethod/PublishMessageTriggeredMethod.cs
+++ Examples/xc.slack/slackproject/SlackProxy/TriggeredMethod/PublishMessageTriggeredMethod.cs
@@ -1,45 +1,57 @@
-
+using System;
+using XComponent.Common.ApiContext;
+using XComponent.Common.Timeouts;
+using XComponent.SlackProxy.Common;
+using XComponent.SlackProxy.Common.Senders;
 using XComponent.SlackProxy.TriggeredMethod.SlackApi;
 
 namespace XComponent.SlackProxy.TriggeredMethod
 {
-	using System;
-	using XComponent.Common.ApiContext;
-	using XComponent.Common.Timeouts;
-	using XComponent.SlackProxy.Common.Senders;
-	using XComponent.SlackProxy.Common;
+    using System;
+    using XComponent.Common.ApiContext;
+    using XComponent.Common.Timeouts;
+    using XComponent.SlackProxy.Common.Senders;
+    using XComponent.SlackProxy.Common;
 
 
-	public static class PublishMessageTriggeredMethod
-	{
-		public static void ExecuteOn_Publishing_Through_SendMessage(XComponent.SlackProxy.UserObject.SendMessage sendMessage, XComponent.SlackProxy.UserObject.PublishMessage publishMessage, object object_InternalMember, Context context, ISendMessageSendMessageOnPublishingPublishMessageSenderInterface sender)
-		{
-			try {
-				SlackPublisher slackPublisher = new SlackPublisher();
-				slackPublisher.UrlWithToken = sendMessage.SlackUrlWithToken;
-				slackPublisher.Channel = sendMessage.SlackChannel;
-				slackPublisher.SlackUser = sendMessage.SlackUser;
-				if (!string.IsNullOrEmpty(sendMessage.Color)) {
-					slackPublisher.Color = sendMessage.Color;
-				}
-				if (!string.IsNullOrEmpty(sendMessage.Text)) {
-					slackPublisher.Text = sendMessage.Text;
-				}
-				if (!string.IsNullOrEmpty(sendMessage.IconEmoji)) {
-					slackPublisher.IconEmoji = sendMessage.IconEmoji;
-				}
-				if (!string.IsNullOrEmpty(sendMessage.MessageImage)) {
-					slackPublisher.MessageImage = sendMessage.MessageImage;
-				}
-				if (!string.IsNullOrEmpty(sendMessage.MessageTitle)) {
-					slackPublisher.MessageTitle = sendMessage.MessageTitle;
-				}
-				slackPublisher.SendMessage();
-				sender.Success(context);
-			} catch (Exception ex) {
-				publishMessage.Message = ex.ToString();
-				sender.Error(context);
-			}
-		}
-	}
+    public static class PublishMessageTriggeredMethod
+    {
+        public static void ExecuteOn_Publishing_Through_SendMessage(XComponent.SlackProxy.UserObject.SendMessage sendMessage, XComponent.SlackProxy.UserObject.PublishMessage publishMessage, object object_InternalMember, Context context, ISendMessageSendMessageOnPublishingPublishMessageSenderInterface sender)
+        {
+            try
+            {
+                SlackPublisher slackPublisher = new SlackPublisher();
+                slackPublisher.UrlWithToken = sendMessage.SlackUrlWithToken;
+                slackPublisher.Channel = sendMessage.SlackChannel;
+                slackPublisher.SlackUser = sendMessage.SlackUser;
+                if (!string.IsNullOrEmpty(sendMessage.Color))
+                {
+                    slackPublisher.Color = sendMessage.Color;
+                }
+                if (!string.IsNullOrEmpty(sendMessage.Text))
+                {
+                    slackPublisher.Text = sendMessage.Text;
+                }
+                if (!string.IsNullOrEmpty(sendMessage.IconEmoji))
+                {
+                    slackPublisher.IconEmoji = sendMessage.IconEmoji;
+                }
+                if (!string.IsNullOrEmpty(sendMessage.MessageImage))
+                {
+                    slackPublisher.MessageImage = sendMessage.MessageImage;
+                }
+                if (!string.IsNullOrEmpty(sendMessage.MessageTitle))
+                {
+                    slackPublisher.MessageTitle = sendMessage.MessageTitle;
+                }
+                slackPublisher.SendMessage();
+                sender.Success(context);
+            }
+            catch (Exception ex)
+            {
+                publishMessage.Message = ex.ToString();
+                sender.Error(context);
+            }
+        }
+    }
 }
