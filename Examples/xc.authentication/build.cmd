194b0d22a4559b8868806d35a5026f3b64ef0224
Yahya NAJAR <yahya.najar@gmail.com>
2017-03-14 12:29:19 +0100

    xc.authentication : 'Nuget restore' for components added

--- Examples/xc.authentication/build.cmd
+++ Examples/xc.authentication/build.cmd
@@ -8,6 +8,8 @@ pushd %~dp0
 
 ..\Tools\NuGet.exe install FAKE -ConfigFile ..\Tools\Nuget.Config -ExcludeVersion -OutputDirectory ..\packages -Version 4.10.3
 ..\Tools\NuGet.exe install XComponent.Community -ConfigFile ..\Tools\Nuget.Config -ExcludeVersion -OutputDirectory ..\packages -Version %xcversion%
+..\Tools\NuGet.exe restore authenticationservice\Authentication\Authentication.sln
+..\Tools\NuGet.exe restore authenticationservice\AuthenticationConsoleApp\AuthenticationConsoleApp.sln
 
 set encoding=utf-8
 ..\packages\FAKE\tools\FAKE.exe build.fsx All
