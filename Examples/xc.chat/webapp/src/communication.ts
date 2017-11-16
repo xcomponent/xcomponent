2bfb67a6cd2ec97c952b7c29b8168ba11ebd6923
Marcos Almeida <marcos.almeida@xcomponent.com>
2017-11-16 10:49:45 +0100

    Using HistoryManager microservice to retrieve history upon connection

--- Examples/xc.chat/webapp/src/communication.ts
+++ Examples/xc.chat/webapp/src/communication.ts
@@ -2,6 +2,7 @@ import { promiseSession } from "xcomponent";
 import { Dispatch } from "redux";
 import { addRoomEvent, removeRoomEvent, addMessageEvent } from "actions";
 import { RoomsState, Room } from "reducers/rooms";
+import * as uuid from "uuid";
 
 export const startListener = (dispatch: Dispatch<RoomsState>, host: string, port: number) => {
     promiseSession(host, port).then(session => {
@@ -13,6 +14,46 @@ export const startListener = (dispatch: Dispatch<RoomsState>, host: string, port
                 items.forEach(function(chatRoom) {
                     if (chatRoom.stateMachineRef.StateName === "Created") {
                         dispatch(addRoomEvent(chatRoom.jsonMessage.Name, chatRoom.stateMachineRef));
+
+                        let publisher = session.createPublisher();
+                        let historyManagerComponent = "HistoryManager";
+                        let historyManagerStateMachine = "HistoryManager";
+                        let publishedHistoryStateMachine = "PublishedHistory";
+                        let historyRequestType = "XComponent.HistoryManager.UserObject.HistoryRequest";
+                        let historyResponseType = "XComponent.HistoryManager.UserObject.PublishedHistory";
+
+                        console.log("Trying to retrieve room history...", chatRoom.jsonMessage.Name);
+                        if (publisher.canPublish(historyManagerComponent, historyManagerStateMachine, historyRequestType)) {
+                            let privateTopic = uuid();
+
+                            console.log("Creating private topic for response...", privateTopic);
+                            session.addPrivateTopic(privateTopic);
+                            if (subscriber.canSubscribe(historyManagerComponent, publishedHistoryStateMachine)) {
+                                subscriber
+                                    .getStateMachineUpdates(historyManagerComponent, publishedHistoryStateMachine)
+                                    .subscribe(jsonData => {
+                                        console.log("Received json data", jsonData);
+                                        let messages: {Room: string, User: string, Message: string, DateTime: string}[] = jsonData.jsonMessage.Messages;
+
+                                        messages.forEach(message => {
+                                            dispatch(addMessageEvent(message.Room, message.DateTime, message.Message, message.User));
+                                        });
+
+                                        console.log("Removing private topic... ", privateTopic);
+                                        session.removePrivateTopic(privateTopic);
+                                    });
+                            } else {
+                                console.error("History manager subscriber not available on the API!");
+                            }
+
+                            console.log("Sending history request...");
+                            publisher.send(historyManagerComponent, historyManagerStateMachine, historyRequestType, {
+                                "RoomName": chatRoom.jsonMessage.Name,
+                                "ResponseTopic": privateTopic
+                            });
+                        } else {
+                            console.error("History manager publisher not available on the API!");
+                        }
                     }
                 });
             });
